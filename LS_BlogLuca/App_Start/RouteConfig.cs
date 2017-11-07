0b56f8d25e55dcd405a3ca702dd96df72f2ac538
githublully1972 <luca.schiavon@alice.it>
2017-11-07 10:58:38 +0100

    fine dell'integrazione back

--- LS_BlogLuca/App_Start/RouteConfig.cs
+++ LS_BlogLuca/App_Start/RouteConfig.cs
@@ -24,7 +24,8 @@ namespace LS_BlogLuca
             name: "Home_Localized",
             url: "{lang}/Home",
             defaults: new { controller = "Home", action = "Index" },
-            constraints: new { lang = "it|en" }
+            constraints: new { lang = "it|en" },
+             namespaces: new string[] { "LS_BlogLuca.Controllers" }
         );
 
             routes.MapRoute(
@@ -37,7 +38,8 @@ namespace LS_BlogLuca
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                   namespaces: new string[] { "LS_BlogLuca.Controllers" }
             );
 
           
---
+++ LS_BlogLuca/Areas/Backoffice/BackofficeAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace LS_BlogLuca.Areas.Backoffice
+{
+    public class BackofficeAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Backoffice";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+               "Backoffice_Home",
+               "Backoffice/Home/{action}/{id}",
+               new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+               namespaces: new string[] { "LS_BlogLuca.Areas.Backoffice.Controllers" }
+           );
+            context.MapRoute(
+                "Backoffice_default",
+                "Backoffice/{controller}/{action}/{id}",
+                new { controller="Login", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
